8041cfd1a218b65407f72efc6103aaa7fdff1e48
Ajay <ajayrathour8267@gmail.com>
2022-06-06 08:06:29 +0530

    for slider

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "header-right") {
-      x.className += " responsive";
-    } else {
-      x.className = "header-right";
-    }
-  }
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
